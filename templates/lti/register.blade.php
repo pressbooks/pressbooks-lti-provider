297fdf3bd66c47ca7959c46511a63fdf6d787120
Ned Zimmerman <ned@bight.ca>
2018-05-08 12:38:56 -0300

    Adjust messages, use links instead of pseudo-buttons.

--- templates/lti/register.blade.php
+++ templates/lti/register.blade.php
@@ -24,22 +24,11 @@
             font-weight: 500;
             line-height: 26.4px;
         }
-
-        .button {
-            font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
-            text-decoration: none;
-            background-color: #EEEEEE;
-            color: #333333;
-            padding: 2px 6px 2px 6px;
-            border: 1px solid #CCCCCC;
-            border-right-color: #333333;
-            border-bottom-color: #333333;
-        }
     </style>
 </head>
 <body>
-<h1>{{ __('Registration request for', 'pressbooks-lti-provider') }}: {{ $title }}</h1>
+<h1>{{ sprintf(__('Registration request for %s', 'pressbooks-lti-provider'), $title) }}</h1>
 
-<p><a href='{!! $success_url !!}' class='button'>{{ __('Register', 'pressbooks-lti-provider') }}</a> <a href='{!! $cancel_url !!}' class='button'>{{ __('Cancel', 'pressbooks-lti-provider') }}</a></p>
+<p><a href='{!! $cancel_url !!}'>{{ __('Go back', 'pressbooks-lti-provider') }}</a> <a href='{!! $success_url !!}'>{{ __('Proceed with registration &rarr;', 'pressbooks-lti-provider') }}</a></p>
 </body>
 </html>
