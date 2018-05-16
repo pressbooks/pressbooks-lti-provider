14a534593357d99b5bec110398c0d94f90f790e7
Dac Chartrand <dac.chartrand@gmail.com>
2018-05-16 14:38:28 -0400

    Some other template stubs
    https://github.com/SteelWagstaff/candela-thin-exports

---
+++ templates/thincc/1.3/assignment.blade.php
@@ -0,0 +1,11 @@
+<assignment xmlns="http://www.imsglobal.org/xsd/imscc_extensions/assignment"
+            xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+            xsi:schemaLocation="http://www.imsglobal.org/xsd/imscc_extensions/assignment http://www.imsglobal.org/profile/cc/cc_extensions/cc_extresource_assignmentv1p0_v1p0.xsd http://www.imsglobal.org/xsd/imscc_extensions/assignment "
+            identifier="{{ $identifier }}">
+    <title>{{ $title }}</title>
+    <text texttype="text/html">{{ $content }}</text>
+    <gradable points_possible="10">true</gradable>
+    <submission_formats>
+        <format type="external_tool"/>
+    </submission_formats>
+</assignment>
