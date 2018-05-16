b2a96a0a650dea03f118c995b538835596329b9f
Ned Zimmerman <ned@bight.ca>
2018-05-16 11:37:11 -0300

    Use path.join.

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,7 +1,8 @@
 let mix = require( 'laravel-mix' );
+let path = require( 'path' );
 
 mix
-	.setPublicPath( 'assets/dist' )
+	.setPublicPath( path.join( 'assets', 'dist' ) )
 	.js( 'assets/src/scripts/pressbooks-lti-provider.js', 'assets/dist/scripts/' )
 	.sass( 'assets/src/styles/pressbooks-lti-provider.scss', 'assets/dist/styles/' )
 	.copyDirectory( 'assets/src/images', 'assets/dist/images' )
