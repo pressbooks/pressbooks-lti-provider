a8a2a138eb4da9c6d16a211def6dff75fc0fc623
Dac Chartrand <dac.chartrand@gmail.com>
2018-05-24 12:45:19 -0400

    Validate assignment with canvas platform extensions

--- templates/thincc/1_3/assignment.blade.php
+++ templates/thincc/1_3/assignment.blade.php
@@ -1,17 +1,18 @@
 <?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
 <assignment xmlns="http://www.imsglobal.org/xsd/imscc_extensions/assignment"
+            xmlns:canvas="http://canvas.instructure.com/xsd/cccv1p0"
             xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-            xsi:schemaLocation="http://www.imsglobal.org/xsd/imscc_extensions/assignment http://www.imsglobal.org/profile/cc/cc_extensions/cc_extresource_assignmentv1p0_v1p0.xsd http://www.imsglobal.org/xsd/imscc_extensions/assignment "
+            xsi:schemaLocation="http://www.imsglobal.org/xsd/imscc_extensions/assignment http://www.imsglobal.org/profile/cc/cc_extensions/cc_extresource_assignmentv1p0_v1p0.xsd "
             identifier="{{ $identifier }}">
     <title>{{ $title }}</title>
-    <text texttype="text/html"></text>
+    <text texttype="text/plain"></text>
     <gradable points_possible="{{ $points_possible }}">true</gradable>
     <submission_formats>
-        <format type="external_tool"/>
+        <format type="url"/>
     </submission_formats>
     <extensions platform="canvas">
-        <assignment>
-            <external_tool_url>{{ $url }}</external_tool_url>
-        </assignment>
+        <canvas:assignment>
+            <canvas:external_tool_url>{{ $url }}</canvas:external_tool_url>
+        </canvas:assignment>
     </extensions>
 </assignment>
