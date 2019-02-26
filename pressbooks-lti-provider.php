193c0c0d987e2075df8ac08c8a860f53a97e52e1
Dac Chartrand <dac.chartrand@gmail.com>
2019-02-26 14:50:23 -0500

    Serenity now.

--- pressbooks-lti-provider.php
+++ pressbooks-lti-provider.php
@@ -3,7 +3,7 @@
 Plugin Name: Pressbooks LTI Provider
 Plugin URI: https://pressbooks.org
 Description: A plugin which turns Pressbooks into an LTI provider.
-Version: 1.1.3
+Version: 1.2.0
 Author: Pressbooks (Book Oven Inc.)
 Author URI: https://pressbooks.org
 Requires PHP: 7.1
